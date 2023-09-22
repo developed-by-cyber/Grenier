6e51d7fd2612b35438d91653d054eb6a111fc9c3
dannyhack <www.ebukadanielibekwe@gmail.com>
2023-09-22 12:47:24 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,38 @@
-let nav = document.querySelector("nav");
+let navs = document.querySelectorAll(".nav");
 let lists = document.querySelectorAll("nav ul li");
 let logo = document.querySelector("nav .logo");
 let bt1 =  document.querySelector("nav .btns button");
 let bt2 =  document.querySelector("nav .btns button:last-child");
 window.addEventListener("scroll", () =>{
     if(scrollY > 90){
-        nav.classList.add("sticky");
-        lists.forEach(lis =>{
-            lis.style.color= "white";
+        navs.forEach(nav =>{
+            nav.classList.add("sticky");
         })
-        // logo.style.color= "white";
         bt1.classList.add("login");
         bt2.classList.add("reg");
     }else{
-        nav.classList.remove("sticky");
-        lists.forEach(lis =>{
-            lis.style.color= "#233A95";
+        navs.forEach(nav =>{
+            nav.classList.remove("sticky");
         })
         // logo.style.color= "#233A95";
         bt1.classList.remove("login");
         bt2.classList.remove("reg");
     }
 })
-console.log(bt1);
+
+// let show = document.querySelector(".show");
+// let hide = document.querySelector(".hide");
+let menu = document.querySelector(".side-menu");
+
+function show(){
+    menu.classList.add("show");
+}
+
+function hide(){
+    menu.classList.remove("show");
+}
+function helps(){
+    let help =  document.querySelector(".shop1 .left");
+    help.classList.toggle("helps");
+    
+}
